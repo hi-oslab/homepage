98bbb37bd51b797d2ee315c1597f81b562961b82
designerSejinOH <ohsj1999@naver.com>
2023-07-27 12:48:10 +0900

    Add iframe

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -9,30 +9,42 @@ export default function Page(props) {
     <>
       {page === 'index' ? (
         <>
-          <div className=' w-full h-screen flex justify-center items-center flex-col '>
-            <button
-              className=' text-white p-2 text-3xl hover:text-gray-400'
-              onClick={() => {
-                setPage('home')
-              }}>
-              <p className='text-xs'>Hongik univ. Interactive media art crew</p>
-              {props.title}
-            </button>
-            <div className='flex flex-row gap-4'>
+          <div className=' w-full h-screen flex justify-center items-center gap-10 flex-col md:flex-row '>
+            <div className='flex flex-col justify-center items-center '>
               <button
+                className=' p-2 text-3xl hover:text-gray-400'
                 onClick={() => {
-                  window.open('https://www.instagram.com/opensource_lab/')
-                }}
-                className='text-3xl hover:text-gray-400 '>
-                <AiOutlineInstagram />
+                  setPage('home')
+                }}>
+                <p className='text-xs'>Hongik univ. Interactive media art crew</p>
+                {props.title}
               </button>
-              <button
-                onClick={() => {
-                  window.location.href = 'mailto:hi.oslab@gmail.com'
-                }}
-                className='text-3xl hover:text-gray-400 '>
-                <AiOutlineMail />
-              </button>
+              <div className='flex flex-row gap-4'>
+                <button
+                  onClick={() => {
+                    window.open('https://www.instagram.com/opensource_lab/')
+                  }}
+                  className='text-3xl hover:text-gray-400 '>
+                  <AiOutlineInstagram />
+                </button>
+                <button
+                  onClick={() => {
+                    window.location.href = 'mailto:hi.oslab@gmail.com'
+                  }}
+                  className='text-3xl hover:text-gray-400 '>
+                  <AiOutlineMail />
+                </button>
+              </div>
+            </div>
+            <div className=' w-64 md:w-0 h-0 md:h-64 border-[0.5px]'></div>
+            <div className='w-80  h-60 flex flex-col justify-center items-center border'>
+              <iframe
+                src='https://hi-oslab.github.io/'
+                width='100%'
+                height='100%'
+                frameBorder='0'
+                allowTransparency={true}
+                allow='encrypted-media'></iframe>
             </div>
           </div>
         </>
